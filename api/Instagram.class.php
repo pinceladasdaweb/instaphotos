3a1bfd7cdb406524da3e62febe311a9a7723afd1
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-12-03 15:17:57 -0200

    Fix indentation

--- api/Instagram.class.php
+++ api/Instagram.class.php
@@ -12,40 +12,40 @@ class Instagram {
     }
 
     public function getUserId( $username ) {
-    	$endpoint = $this->_api_root . 'search?q=' . $username . '&access_token='. $this->_token;
-    	$request  = $this->_executeRequest( $endpoint );
+        $endpoint = $this->_api_root . 'search?q=' . $username . '&access_token='. $this->_token;
+        $request  = $this->_executeRequest( $endpoint );
 
-		foreach(json_decode($request)->data as $user){
-			$userid = $user->id;
-		}
+        foreach(json_decode($request)->data as $user){
+            $userid = $user->id;
+        }
 
-    	return $userid;
+        return $userid;
     }
 
     public function getUserPhotos( $id, $count ) {
-    	$endpoint = $this->_api_root . $id . self::ENDPOINT_USERS . '?access_token='. $this->_token . '&count=' . $count;
+        $endpoint = $this->_api_root . $id . self::ENDPOINT_USERS . '?access_token='. $this->_token . '&count=' . $count;
 
-    	return $this->_executeRequest( $endpoint );
+        return $this->_executeRequest( $endpoint );
     }
 
-	protected function _executeRequest($url) {
-		$user_agent = "Instagram API/PHP";
+    protected function _executeRequest($url) {
+        $user_agent = "Instagram API/PHP";
 
-		$ch = curl_init();
+        $ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Accept: application/json', 'Content-Type: multipart/form-data', 'Expect:' ));
-		curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT_DEFAULT_SEC);
-		curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_HTTPGET, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Accept: application/json', 'Content-Type: multipart/form-data', 'Expect:' ));
+        curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT_DEFAULT_SEC);
+        curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPGET, 1);
 
-		$response = curl_exec($ch);
-		return $response;
+        $response = curl_exec($ch);
+        return $response;
 
-		curl_close($ch);
-	}
+        curl_close($ch);
+    }
 }
