a8fde9c127fd3c6008d5fe035514b106ee6598ce
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-12-18 16:17:03 -0200

    Code improvements

--- request.php
+++ request.php
@@ -2,7 +2,7 @@
 require_once( './api/Instagram.class.php' );
 
 $username = isset($_GET['user']) ? $_GET['user'] : NULL;
-$count    = isset($_GET['count']) ? $_GET['count'] : 12;
+$count    = isset($_GET['count']) && is_numeric($_GET['count']) ? $_GET['count'] : 12;
 $token    = '<PUT YOUR TOKEN HERE>';
 
 $api = new Instagram( $token );
